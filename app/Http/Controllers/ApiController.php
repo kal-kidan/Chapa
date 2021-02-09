c9054cf8d213bec6295a73acebab3abdf5dac008
kal-kidan <kal05627356@gmail.com>
2021-02-08 20:40:26 -1200

    route renamed, message returned

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,22 +7,21 @@ use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
-    public function soap($age){
+    public function soapRequest($age){
      try {
-        $xml = '<?xml version="1.0" encoding="utf-8"?>
-        <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        $xml = "<?xml version='1.0' encoding='utf-8'?>
+        <soap:Envelope xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'>
           <soap:Body>
-            <NumberToWords xmlns="http://www.dataaccess.com/webservicesserver/">
-              <ubiNum>22</ubiNum>
+            <NumberToWords xmlns='http://www.dataaccess.com/webservicesserver/'>
+              <ubiNum>$age</ubiNum>
             </NumberToWords>
           </soap:Body>
-        </soap:Envelope>';
+        </soap:Envelope>";
         $response = Http::withHeaders(['Content-Type' => 'text/xml; charset=utf-8'])->send('POST', 'http://www.dataaccess.com/webservicesserver/numberconversion.wso', [
             'body' => $xml,
         ]);
-        $age =  $response->body();
-        return "your age is $age";
-        //return ["message" => "Hi, your age is $age"];
+        $body =  $response->body();  
+        return "your age is $body"; 
      } catch (Exception $e) {
          echo $e->getMessage();
      }
